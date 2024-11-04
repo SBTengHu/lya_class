c60d19f61479af46670602054c7f94b94024a5af
SBTengHu <tenghu@physics.ucsb.edu>
2024-11-04 15:21:00 +0100

    minor update

--- plot_halo_z2.py
+++ plot_halo_z2.py
@@ -13,6 +13,8 @@ from astropy.utils import NumpyRNGContext
 from collections import Counter
 import spectres
 import pandas as pd
+from IPython import embed
+
 
 cosmo = FlatLambdaCDM(H0=67.74, Om0=0.3089)
 
@@ -308,9 +310,357 @@ def wave_rebin(bins, wavelength):
 
     return wavelength_rebin, deltav
 
+# Importing Subhalo data
 
-test test
+f = ascii.read('/data/forest/dsantos/DylanSims/Data/z2/SubhaloInfo.csv',format='csv')
+
+subhalo_posx = np.array(f['Subhalo_PosX']) # Subhalo positions in x
+subhalo_posy = np.array(f['Subhalo_PosY']) # Subhalo positions in y
+subhalo_posz = np.array(f['Subhalo_PosZ']) # Subhalo positions in z
+subhalo_mass = np.array(f['Subhalo_Mass']) # Subhalo mass. To convert to physical mass you have to multiply by 1e10/H0
+subhalo_radhm = np.array(f['Subhalo_HalfMassRadius']) # Subhalo half mass radius. Twice this is the virial radius.
+subhalo_z = np.array(f['Subhalo_GasMetal']) # Subhalo gas metallicity
+subhalo_vz = np.array(f['Subhalo_PVZ']) # Subhalo peculiar velocity in z axis (km/s)
+subhalo_vdisp = np.array(f['Subhalo_VDispersion']) # Subhalo velocity dispersion (km/s)
+subhalo_vmax = np.array(f['Subhalo_VMax']) # Subhalo maximum velocity of the rotation curve (km/s)
 
 
 
 
+f = ascii.read('/data/forest/dsantos/DylanSims/Data/z2/GroupInfo.csv',format='csv')
+
+group_posx = np.array(f['Group_CMX']) # Group positions in x
+group_posy = np.array(f['Group_CMY']) # Group positions in y
+group_posz = np.array(f['Group_CMZ']) # Group positions in z
+group_mass = np.array(f['Group_Mass']) # Group Mass
+group_z = np.array(f['Group_Metal']) # Group Metallicity
+group_vrad = np.array(f['Group_RCrit200']) # Group virial radius
+group_subhaloid = np.array(f['Subhalo_ID']) # Central Subhalo ID
+
+mass_filter = (np.log10(group_mass*1e10/0.6774) > 9.05)
+
+
+f = h5py.File('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/spectra_TNG50-1_z2.0_n2000d2-rndfullbox_SDSS-BOSS_HI_combined.hdf5', 'r')
+wavelength = np.array(f['wave'])
+
+# Rebinning wavelengths and each spectral binning velocity
+wave_rebins = [wave_rebin(2, wavelength)[0], wave_rebin(3, wavelength)[0], wave_rebin(4, wavelength)[0], wave_rebin(6, wavelength)[0]]
+vel = [wave_rebin(2, wavelength)[1], wave_rebin(3, wavelength)[1], wave_rebin(4, wavelength)[1], wave_rebin(6, wavelength)[1]]
+vel_str = [str(round(elements,0)) for elements in vel]
+
+# List of all SBLAs per flux and spectral binning
+with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/SBLA_025_AllBins_Index.fits') as hdul:
+    sbla_025 = hdul[1].data
+
+with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/SBLA_015_AllBins_Index.fits') as hdul:
+    sbla_015 = hdul[1].data
+
+with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/SBLA_005_AllBins_Index.fits') as hdul:
+    sbla_005 = hdul[1].data
+
+sbla_lists = [sbla_025, sbla_015, sbla_005]
+
+# List of SBLAs in Halos
+
+sbla025_vmax_vrad = {}
+sbla015_vmax_vrad = {}
+sbla005_vmax_vrad = {}
+
+for i in trange(len(vel_str)):
+    with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/Index/SBLA025_' + vel_str[
+        i] + '_vmax_vrad.fits') as hdul:
+        data = hdul[1].data
+        t1 = data['Subhalo Index']
+        t2 = data['SBLA Indices']
+
+    if i == 0:
+        sbla025_vmax_vrad['Halo Index'] = [t1]
+        sbla025_vmax_vrad['SBLAs Indices'] = [t2]
+    else:
+        sbla025_vmax_vrad['Halo Index'].append(t1)
+        sbla025_vmax_vrad['SBLAs Indices'].append(t2)
+
+    with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/Index/SBLA015_' + vel_str[
+        i] + '_vmax_vrad.fits') as hdul:
+        data = hdul[1].data
+        t1 = data['Subhalo Index']
+        t2 = data['SBLA Indices']
+
+    if i == 0:
+        sbla015_vmax_vrad['Halo Index'] = [t1]
+        sbla015_vmax_vrad['SBLAs Indices'] = [t2]
+    else:
+        sbla015_vmax_vrad['Halo Index'].append(t1)
+        sbla015_vmax_vrad['SBLAs Indices'].append(t2)
+
+    with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/Index/SBLA005_' + vel_str[
+        i] + '_vmax_vrad.fits') as hdul:
+        data = hdul[1].data
+        t1 = data['Subhalo Index']
+        t2 = data['SBLA Indices']
+
+    if i == 0:
+        sbla005_vmax_vrad['Halo Index'] = [t1]
+        sbla005_vmax_vrad['SBLAs Indices'] = [t2]
+    else:
+        sbla005_vmax_vrad['Halo Index'].append(t1)
+        sbla005_vmax_vrad['SBLAs Indices'].append(t2)
+
+sbla025_vdisp_vrad = {}
+sbla015_vdisp_vrad = {}
+sbla005_vdisp_vrad = {}
+
+for i in trange(len(vel_str)):
+    with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/Index/SBLA025_' + vel_str[
+        i] + '_vdisp_vrad.fits') as hdul:
+        data = hdul[1].data
+        t1 = data['Subhalo Index']
+        t2 = data['SBLA Indices']
+
+    if i == 0:
+        sbla025_vdisp_vrad['Halo Index'] = [t1]
+        sbla025_vdisp_vrad['SBLAs Indices'] = [t2]
+    else:
+        sbla025_vdisp_vrad['Halo Index'].append(t1)
+        sbla025_vdisp_vrad['SBLAs Indices'].append(t2)
+
+    with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/Index/SBLA015_' + vel_str[
+        i] + '_vdisp_vrad.fits') as hdul:
+        data = hdul[1].data
+        t1 = data['Subhalo Index']
+        t2 = data['SBLA Indices']
+
+    if i == 0:
+        sbla015_vdisp_vrad['Halo Index'] = [t1]
+        sbla015_vdisp_vrad['SBLAs Indices'] = [t2]
+    else:
+        sbla015_vdisp_vrad['Halo Index'].append(t1)
+        sbla015_vdisp_vrad['SBLAs Indices'].append(t2)
+
+    with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/Index/SBLA005_' + vel_str[
+        i] + '_vdisp_vrad.fits') as hdul:
+        data = hdul[1].data
+        t1 = data['Subhalo Index']
+        t2 = data['SBLA Indices']
+
+    if i == 0:
+        sbla005_vdisp_vrad['Halo Index'] = [t1]
+        sbla005_vdisp_vrad['SBLAs Indices'] = [t2]
+    else:
+        sbla005_vdisp_vrad['Halo Index'].append(t1)
+        sbla005_vdisp_vrad['SBLAs Indices'].append(t2)
+
+sbla025_vmax_vrad = pd.DataFrame.from_dict(sbla025_vmax_vrad)
+sbla015_vmax_vrad = pd.DataFrame.from_dict(sbla015_vmax_vrad)
+sbla005_vmax_vrad = pd.DataFrame.from_dict(sbla005_vmax_vrad)
+
+sbla025_vdisp_vrad = pd.DataFrame.from_dict(sbla025_vdisp_vrad)
+sbla015_vdisp_vrad = pd.DataFrame.from_dict(sbla015_vdisp_vrad)
+sbla005_vdisp_vrad = pd.DataFrame.from_dict(sbla005_vdisp_vrad)
+
+# Lists of SBLA properties
+prop_files = ['/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/SBLAProps_025_vmax.fits',
+              '/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/SBLAProps_015_vmax.fits',
+              '/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/SBLAProps_005_vmax.fits',
+             '/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/SBLAProps_025_vdisp.fits',
+              '/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/SBLAProps_015_vdisp.fits',
+              '/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/SBLAProps_005_vdisp.fits']
+
+# Importing covering fractions
+
+with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/CovFraction_vmax_025.fits') as hdul:
+        cov_fraction_vmax_025 = hdul[1].data
+
+with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/CovFraction_vmax_015.fits') as hdul:
+        cov_fraction_vmax_015 = hdul[1].data
+
+with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/CovFraction_vmax_005.fits') as hdul:
+        cov_fraction_vmax_005 = hdul[1].data
+
+with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/CovFraction_vdisp_025.fits') as hdul:
+        cov_fraction_vdisp_025 = hdul[1].data
+
+with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/CovFraction_vdisp_015.fits') as hdul:
+        cov_fraction_vdisp_015 = hdul[1].data
+
+with fits.open('/data/forest/dsantos/DylanSims/Data/z2/SDSS/Random/DLA_SBLA/CovFraction_vdisp_005.fits') as hdul:
+        cov_fraction_vdisp_005 = hdul[1].data
+
+bins = []
+mass_in = []
+mass_f = []
+temp_mass = 9.
+
+while temp_mass < max(np.log10(group_mass[mass_filter] * 1e10 / 0.6774)):
+    mass_in.append(temp_mass)
+    bin_temp_vmax = np.where((np.log10(group_mass[mass_filter] * 1e10 / 0.6774) >= temp_mass) & \
+                             (np.log10(group_mass[mass_filter] * 1e10 / 0.6774) < temp_mass + 0.4))
+    temp_mass += 0.4
+
+    bins.append(bin_temp_vmax[0])
+    mass_f.append(temp_mass)
+
+mass_in = np.array(mass_in)
+mass_f = np.array(mass_f)
+
+fig = plt.figure(figsize=(7, 5))
+
+halos_per_bin = []
+
+for i in bins:
+    halos_per_bin.append(len(i))
+
+plt.plot((mass_in + mass_f) / 2, np.log10(halos_per_bin), '-k.')
+for i in range(11):
+    plt.text((mass_in[i] + mass_f[i]) / 2, np.log10(halos_per_bin[i]), str(halos_per_bin[i]))
+
+plt.xlabel('Halo Mass', fontsize=15)
+plt.ylabel(r'$\log_{10}$(Halo Counts)', fontsize=15)
+
+fig.tight_layout()
+plt.subplots_adjust(wspace=0.15, hspace=0)
+plt.savefig('z2_plots/AllHaloBins.pdf', bbox_inches='tight', dpi=300)
+plt.savefig('z2_plots/AllHaloBins.png', bbox_inches='tight', dpi=300)
+plt.show()
+
+bins025_vmax_vrad = mass_bin(mass_in, mass_f, group_mass, sbla025_vmax_vrad['Halo Index'])
+bins015_vmax_vrad = mass_bin(mass_in, mass_f, group_mass,  sbla015_vmax_vrad['Halo Index'])
+bins005_vmax_vrad = mass_bin(mass_in, mass_f, group_mass, sbla005_vmax_vrad['Halo Index'])
+
+bins025_vdisp_vrad = mass_bin(mass_in, mass_f, group_mass, sbla025_vdisp_vrad['Halo Index'])
+bins015_vdisp_vrad = mass_bin(mass_in, mass_f, group_mass,  sbla015_vdisp_vrad['Halo Index'])
+bins005_vdisp_vrad = mass_bin(mass_in, mass_f, group_mass, sbla005_vdisp_vrad['Halo Index'])
+
+all_sblas = []
+
+for k in range(len(sbla_lists)):
+    for j in range(len(sbla_lists[k])):
+        all_sblas.append(len(sbla_lists[k]['SBLA Index'][j]))
+
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(9,4),sharey='all',sharex='all')
+
+vels = [r'$v_{max}$', r'$v_{disp}$']
+
+lstyle = ['-','--','-.',':']
+
+for i in range(1):
+    axes[0].plot(vels, [unique_globalsbla(sbla025_vmax_vrad['SBLAs Indices'][i])/all_sblas[i], unique_globalsbla(sbla025_vdisp_vrad['SBLAs Indices'][i])/all_sblas[i]],\
+                        color='k', linestyle=lstyle[i], label=vel_str[i]+'km/s')
+
+#######################################################################################################################
+
+for i in range(1):
+    axes[1].plot(vels, [unique_globalsbla(sbla015_vmax_vrad['SBLAs Indices'][i])/all_sblas[i+4], unique_globalsbla(sbla015_vdisp_vrad['SBLAs Indices'][i])/all_sblas[i+4]],\
+                        color='b', linestyle=lstyle[i], label=vel_str[i]+'km/s')
+
+# # #######################################################################################################################
+
+for i in range(1):
+    axes[2].plot(vels, [unique_globalsbla(sbla005_vmax_vrad['SBLAs Indices'][i])/all_sblas[i+4*2], unique_globalsbla(sbla005_vdisp_vrad['SBLAs Indices'][i])/all_sblas[i+4*2]],\
+                        color='g', linestyle=lstyle[i], label=vel_str[i]+'km/s')
+
+axes[0].set_ylabel(r'P(SBLA)$_{global}$', fontsize=15)
+
+# axes[0].legend()
+
+for i in range(0,3):
+    axes[i].set_xticklabels(vels, rotation=90, ha='right')
+
+axes[0].set_title(r'$F_{Ly\alpha} < 0.25$')
+axes[1].set_title(r'$F_{Ly\alpha} < 0.15$')
+axes[2].set_title(r'$F_{Ly\alpha} < 0.05$')
+
+fig.tight_layout()
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.savefig('z2_plots/PSBLA_Global_Random.pdf', bbox_inches='tight', dpi=300)
+plt.savefig('z2_plots/PSBLA_Global_Random.png', bbox_inches='tight', dpi=300)
+plt.show()
+
+ig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12,6),sharey='all',sharex='all')
+
+lstyle = ['-','--','-.',':']
+
+unique_vmax_025 = unique_sbla(bins025_vmax_vrad,sbla025_vmax_vrad['SBLAs Indices'])
+fsbla = sbla_pdf(unique_vmax_025,sbla_025, mass_in, mass_f, group_mass, sbla025_vmax_vrad['Halo Index'])
+
+# for i in range(len(fsbla[1])):
+axes[0,0].plot(fsbla[1][0][:-1], fsbla[0][0][:-1], color='k', linestyle=lstyle[0])#, label=vel_str[0]+' km/s')
+# axes[0,0].errorbar(fsbla[1][0][:-1], fsbla[0][0][:-1], yerr=boots_deviation[0][:-1], capsize=0.5, color='k', linestyle='')
+
+###
+
+unique_vmax_015 = unique_sbla(bins015_vmax_vrad,sbla015_vmax_vrad['SBLAs Indices'])
+fsbla = sbla_pdf(unique_vmax_015,sbla_015,mass_in, mass_f, group_mass, sbla015_vmax_vrad['Halo Index'])
+
+# for i in range(len(fsbla[1])):
+axes[0,1].plot(fsbla[1][0][:-1], fsbla[0][0][:-1], color='b', linestyle=lstyle[0], label=vel_str[0]+'km/s')
+# axes[0,1].errorbar(fsbla[1][0][:-1], fsbla[0][0][:-1], yerr=boots_deviation[0+4][:-1], capsize=0.5, color='b', linestyle='')
+
+###
+
+unique_vmax_005 = unique_sbla(bins005_vmax_vrad,sbla005_vmax_vrad['SBLAs Indices'])
+fsbla = sbla_pdf(unique_vmax_005,sbla_005,mass_in, mass_f, group_mass, sbla005_vmax_vrad['Halo Index'])
+
+# for i in range(len(fsbla[1])):
+axes[0,2].plot(fsbla[1][0][:-1], fsbla[0][0][:-1], color='g', linestyle=lstyle[0], label=vel_str[0]+' km/s')
+# axes[0,2].errorbar(fsbla[1][0][:-1], fsbla[0][0][:-1], yerr=boots_deviation[0+4*2][:-1], capsize=0.5, color='g', linestyle='')
+
+# ##########################################################################################################
+
+unique_vdisp_025 = unique_sbla(bins025_vdisp_vrad,sbla025_vdisp_vrad['SBLAs Indices'])
+fsbla = sbla_pdf(unique_vdisp_025,sbla_025,mass_in, mass_f, group_mass, sbla025_vdisp_vrad['Halo Index'])
+
+# for i in range(len(fsbla[1])):
+axes[1,0].plot(fsbla[1][0][:-1], fsbla[0][0][:-1], color='k', linestyle=lstyle[0], label=vel_str[0])
+# axes[1,0].errorbar(fsbla[1][0][:-1], fsbla[0][0][:-1], yerr=boots_deviation[0+4*3][:-1], capsize=0.5, color='k', linestyle='')
+
+###
+
+unique_vdisp_015 = unique_sbla(bins015_vdisp_vrad,sbla015_vdisp_vrad['SBLAs Indices'])
+fsbla = sbla_pdf(unique_vdisp_015,sbla_015,mass_in, mass_f, group_mass, sbla015_vdisp_vrad['Halo Index'])
+
+# for i in range(len(fsbla[1])):
+axes[1,1].plot(fsbla[1][0][:-1], fsbla[0][0][:-1], color='b', linestyle=lstyle[0], label=vel_str[0])
+# axes[1,1].errorbar(fsbla[1][0][:-1], fsbla[0][0][:-1], yerr=boots_deviation[0+4*4][:-1], capsize=0.5, color='b', linestyle='')
+
+###
+
+unique_vdisp_005 = unique_sbla(bins005_vdisp_vrad,sbla005_vdisp_vrad['SBLAs Indices'])
+fsbla = sbla_pdf(unique_vdisp_005,sbla_005,mass_in, mass_f, group_mass, sbla005_vdisp_vrad['Halo Index'])
+
+# for i in range(len(fsbla[1])):
+axes[1,2].plot(fsbla[1][0][:-1], fsbla[0][0][:-1], color='g', linestyle=lstyle[0], label=vel_str[0]+' km/s')
+# axes[1,2].errorbar(fsbla[1][0][:-1], fsbla[0][0][:-1], yerr=boots_deviation[0+4*5][:-1], capsize=0.5, color='g', linestyle='')
+
+label = [r'$v_{max}$', r'$v_{disp}$']
+
+for i in range(2):
+    ax1 = axes[i,2].twinx()
+    ax1.set_yticks([])
+    ax1.set_ylabel(label[i], fontsize=15, rotation=270,labelpad=15)
+
+for i in range(0,3):
+    for j in range(0,2):
+        axes[j,i].axvline(np.log10(1e12/0.6781),color='firebrick',alpha=0.5, linestyle='--', label='SBLA Bias')
+        axes[j,i].axvline(np.log10(4e11/0.6781),color='firebrick',alpha=0.5, label='138 km/s $v_{circ}$')
+
+axes[0,0].set_ylabel('P(SBLA)', fontsize=15)
+axes[1,0].set_ylabel('P(SBLA)', fontsize=15)
+axes[1,0].set_xlabel(r'Halo Mass [log($M_{\odot}$)]', fontsize=15)
+axes[1,1].set_xlabel(r'Halo Mass [log($M_{\odot}$)]', fontsize=15)
+axes[1,2].set_xlabel(r'Halo Mass [log($M_{\odot}$)]', fontsize=15)
+
+axes[0,0].legend(ncol=1)
+
+axes[0,0].set_title(r'$F_{Ly\alpha} < 0.25$')
+axes[0,1].set_title(r'$F_{Ly\alpha} < 0.15$')
+axes[0,2].set_title(r'$F_{Ly\alpha} < 0.05$')
+
+axes[0,0].set_xticks(np.arange(9, 13.1, 0.5))
+axes[0,0].set_xlim(9.1,12.9)
+
+fig.tight_layout()
+plt.subplots_adjust(wspace=0.02, hspace=0.02)
+plt.savefig('z2_plots/PSBLA_Bins_Rand.pdf', bbox_inches='tight', dpi=300)
+plt.savefig('z2_plots/PSBLA_Bins_Rand.png', bbox_inches='tight', dpi=300)
+plt.show()
