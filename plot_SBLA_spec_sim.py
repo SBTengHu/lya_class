7f31159edb9f32d729acd9044adf909f7e6fbe5e
SBTengHu <tenghu@physics.ucsb.edu>
2024-11-27 14:50:49 +0100

    minor update

--- plot_SBLA_spec_sim.py
+++ plot_SBLA_spec_sim.py
@@ -4,31 +4,78 @@ from IPython import embed
 from astropy.cosmology import FlatLambdaCDM
 from astropy.io import fits, ascii
 import h5py
+from scipy.interpolate import interp2d
 
-f = h5py.File('/data/forest/dsantos/DylanSims/Data/z2/KECK/Uniform/spectra_TNG50-1_z2.0_n1000d2-fullbox_KECK-HIRES-B14_HI_combined.hdf5', 'r')
+#cosmological parameters
+c = 3e5  # km/s
+lya = 1215.67  # AA
+h = 0.6774
+OmegaM = 0.3089
+OmegaB = 0.0486
+#starting redshift of the TNG50
+z_0 = 2.0020281392528516
+Lbox = 35  # Mpc/h
+ncell = 1000#len(np.sort(np.unique(f['ray_pos'][:,0])))
+cosmo = FlatLambdaCDM(H0=100.0 * h, Om0=OmegaM, Ob0=OmegaB)
+hubblez = cosmo.H(z_0)
+
+f = h5py.File('/data/forest/dsantos/DylanSims/Data/z2/KECK/Random/spectra_TNG50-1_z2.0_n2000d2-rndfullbox_KECK-HIRES-B14_HI_combined.hdf5', 'r')
+#f = h5py.File('/data/forest/dsantos/DylanSims/Data/z2/KECK/Uniform/spectra_TNG50-1_z2.0_n1000d2-fullbox_KECK-HIRES-B14_HI_combined.hdf5', 'r')
 wavelength_all = np.array(f['wave'])
+
+f1 = ascii.read('/data/forest/dsantos/DylanSims/Data/z2/SubhaloInfo.csv',format='csv')
+
+subhalo_posx = np.array(f1['Subhalo_PosX']) # Subhalo positions in x
+subhalo_posy = np.array(f1['Subhalo_PosY']) # Subhalo positions in y
+subhalo_posz = np.array(f1['Subhalo_PosZ']) # Subhalo positions in z
+subhalo_mass = np.array(f1['Subhalo_Mass']) # Subhalo mass. To convert to physical mass you have to multiply by 1e10/H0
+subhalo_radhm = np.array(f1['Subhalo_HalfMassRadius']) # Subhalo half mass radius. Twice this is the virial radius.
+subhalo_z = np.array(f1['Subhalo_GasMetal']) # Subhalo gas metallicity
+subhalo_vz = np.array(f1['Subhalo_PVZ']) # Subhalo peculiar velocity in z axis (km/s)
+subhalo_vdisp = np.array(f1['Subhalo_VDispersion']) # Subhalo velocity dispersion (km/s)
+ubhalo_vmax = np.array(f1['Subhalo_VMax']) # Subhalo maximum velocity of the rotation curve (km/s)
+
+#f = ascii.read('/data/forest/dsantos/DylanSims/Data/z2/GroupInfo.csv',format='csv')
+#group_posx = np.array(f['Group_CMX']) # Group positions in x
+#group_posy = np.array(f['Group_CMY']) # Group positions in y
+#group_posz = np.array(f['Group_CMZ']) # Group positions in z
+#group_mass = np.array(f['Group_Mass']) # Group Mass
+#group_z = np.array(f['Group_Metal']) # Group Metallicity
+#group_vrad = np.array(f['Group_RCrit200']) # Group virial radius
+#group_subhaloid = np.array(f['Subhalo_ID']) # Central Subhalo ID
+
+mass_filter_0 = (np.log10(subhalo_mass*1e10/0.6774) > 12.45) & (np.log10(subhalo_mass*1e10/0.6774) < 12.55)
+y_pos = subhalo_posy[mass_filter_0][0]
+
+x_grid = np.linspace(0,35000,3*ncell)
+
+#condition if for all subhalos in the plane of the slice
+condition1 = (subhalo_posy - y_pos)**2  <= (2*subhalo_radhm)**2
+condition2 = (np.log10(subhalo_mass*1e10/0.6774) > 9.05)
+
+cond_all = condition1 & condition2
+
+#the radius projected in x-axis
+subhalo_rx= np.zeros(len(subhalo_posx[cond_all]))
+subhalo_rx = np.sqrt(4*subhalo_radhm[cond_all]**2 - (subhalo_posy[cond_all] - y_pos)**2)
+subhalo_dwl = subhalo_vdisp[cond_all]/c * lya
 
 gridspec = {'width_ratios': [1, 0.05]}
 
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(13,5),gridspec_kw=gridspec)
-
-ix = 781436
-
 #make a slice of the simulation box
-
 y_ind = np.where((f['ray_pos'][:,1]==f['ray_pos'][:,1][ix]))[0]
 wl_ind = np.where((wavelength_all<1233*(1+2)) & (wavelength_all>1215.67*(1+2)))[0]
-
 y_pos = f['ray_pos'][:,1][ix]
 #embed()
 #make the intensity plot for the slice
 flux_map = f['flux'][y_ind].astype(np.float16)
 
+"""
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(13,5),gridspec_kw=gridspec)
 im1=axes[0].scatter(f['ray_pos'][:,0],f['ray_pos'][:,1],s=10,c=f['EW_HI_1215'],cmap='binary',rasterized=True,vmax=25)
 #axes[1].plot(f['ray_pos'][:,0][ix],f['ray_pos'][:,1][ix],'b*')
 
 #axes[0].scatter(f['ray_pos'][:,0][y_ind],f['ray_pos'][:,1][y_ind], marker='*', s=0.1, c='b')
-
 cax = axes[1]
 plt.colorbar(im1, cax=cax)
 
@@ -45,44 +92,28 @@ fig.tight_layout()
 savename ='sim_cut.pdf'
 plt.savefig(savename, dpi=100)
 plt.close()
-
-
-#cosmological parameters (not correct)
-c = 3e5  # km/s
-lya = 1215.67  # AA
-h = 0.700
-OmegaM = 0.30
-OmegaB = 0.05
-z = 2.0
-Lbox = 50  # Mpc/h
-ncell = 1000#len(np.sort(np.unique(f['ray_pos'][:,0])))
-cosmo = FlatLambdaCDM(H0=100.0 * h, Om0=OmegaM, Ob0=OmegaB)
-hubblez = cosmo.H(z)
+"""
 
 dz = ((Lbox / h / ncell) * hubblez / c).value # dz in redshift per cell
 dWL = (dz * lya)  # wl in AA per cell
 dl = Lbox / ncell  # dist per cell for raw skewer in Mpc/h
 
-# "subhalo_posx, subhalo_posy, subhalo_posz, subhalo_mass, subhalo_radhm,subhalo_vz, subhalo_vdisp"
-dataf1='/net/CLUSTER/VAULT/users/thu/subhalo'
 
-subhalo_posx, subhalo_posy, subhalo_posz, subhalo_mass, subhalo_radhm,subhalo_vz, subhalo_vdisp\
-    = np.loadtxt(dataf1, unpack=1, dtype='float64')
-# "subhalo_posx, subhalo_posy, subhalo_posz, subhalo_mass, subhalo_radhm,subhalo_vz, subhalo_vdisp"
+mass_filter = (np.log10(group_mass*1e10/0.6774) > 9.05)
 
 #condition if for all subhalos in the plane of the slice
-condition1 = (subhalo_posy - y_pos)**2  <= subhalo_radhm**2
-condition2 = subhalo_mass >= 1e2
+condition1 = (group_posy - y_pos)**2  <= group_vrad**2
+condition2 = mass_filter
 
 cond_all = condition1 #& condition2
 
 
 #convert from z position to wavelength
-z_halo = subhalo_posz/(dl*1000*h)  * dz+ (2.0)
-#wl_halo = (1+z_halo )* lya
-wl_halo = (1+z_halo + subhalo_vz/c )* lya
-wl_halo_min = wl_halo - subhalo_vdisp/c * lya
-wl_halo_max = wl_halo + subhalo_vdisp/c * lya
+z_halo = group_posz/(dl*1000*h)  * dz+ (2.0)
+wl_halo = (1+z_halo )* lya
+#wl_halo = (1+z_halo + subhalo_vz/c )* lya
+#wl_halo_min = wl_halo - subhalo_vdisp/c * lya
+#wl_halo_max = wl_halo + subhalo_vdisp/c * lya
 
 #make the intensity plot for the slice
 
@@ -96,7 +127,7 @@ fig2, axes2 = plt.subplots(nrows=1, ncols=2, figsize=(13,5),gridspec_kw=gridspec
 
 c1 = axes2[0].pcolormesh(x_grid,z_grid , map0, cmap='Blues', vmin=0., vmax=1.)
 
-axes2[0].scatter(subhalo_posx[cond_all],wl_halo[cond_all], marker='*', s=2, c='red')
+axes2[0].scatter(group_posx[cond_all],wl_halo[cond_all], marker='*', s=2, c='red')
 
 axes2[0].set_title('flux')
 # set the limits of the plot to the limits of the data
@@ -111,7 +142,9 @@ axes2[0].set_ylim(z_grid[0],z_grid[-1])
 
 fig2.tight_layout()
 #plt.subplots_adjust(wspace=0.1, hspace=0)
-savename2 =('SBLA_flux_halos.pdf')
+plt.show()
 
+
+savename2 =('SBLA_flux_halos.pdf')
 plt.savefig(savename2, dpi=100)
 plt.close()
